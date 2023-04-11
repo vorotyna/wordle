5be8b0148a418653451e1752753bca62e2ab3182
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-04-11 14:18:22 -0700

    JavaScript: Handle input for enter function/key

--- wordle.js
+++ wordle.js
@@ -4,15 +4,34 @@ const ANSWER_LENGTH = 5;
 
 async function init() {
   let currentGuess = "";
+  let currentRow = 0;
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
+      // Add letter to the end of guess
       currentGuess += letter;
     } else {
+      // Replace the last letter if guess is >= 5 letters
       currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
+    // Display letter
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
+  }
 
-    letters[currentGuess.length - 1].innerText = letter;
+  async function enter() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      // Do nothing
+      return;
+    }
+
+    // TODO Validate the word
+
+    // TODO Do all the marking as "correct", "close", "wrong"
+
+    // TODO Did they win or lose?
+
+    currentRow++;
+    currentGuess = "";
   }
 
   document.addEventListener('keydown', function handleKeyPress(event) {
