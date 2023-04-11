ae946fce0d77038048de1ac3c37c336824a17a6b
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-04-11 14:10:44 -0700

    JavaScript: Handle input for adding a letter

--- wordle.js
+++ wordle.js
@@ -1,8 +1,19 @@
 const letters = document.querySelectorAll('.letter');
 const loadingDiv = document.querySelector('.info-bar');
+const ANSWER_LENGTH = 5;
 
 async function init() {
+  let currentGuess = "";
 
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      currentGuess += letter;
+    } else {
+      currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
+    }
+
+    letters[currentGuess.length - 1].innerText = letter;
+  }
 
   document.addEventListener('keydown', function handleKeyPress(event) {
     const action = event.key;
@@ -18,5 +29,10 @@ async function init() {
     }
   });
 }
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
 
 init();
