e7ced8c4e1e44fe9051b0d69f390c8369b6bb475
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-04-11 23:10:03 -0700

    Handle correct, close and wrong letters

--- wordle.js
+++ wordle.js
@@ -9,8 +9,8 @@ async function init() {
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
+  const wordParts = word.split("");
   setLoading(false);
-
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
@@ -33,6 +33,32 @@ async function init() {
     // TODO Validate the word
 
     // TODO Do all the marking as "correct", "close", "wrong"
+
+    const guessParts = currentGuess.split("");
+    const map = makeMap(wordParts);
+    console.log(map);
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      // Mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      if (guessParts[i] === wordParts[i]) {
+        // Do nothing, we already did it
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        // Mark as close
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
+
+
 
     // TODO Did they win or lose?
 
@@ -68,6 +94,19 @@ function isLetter(letter) {
 function setLoading(isLoading) {
   // If isLoading is not true, it will add 'hidden' class
   loadingDiv.classList.toggle('hidden', !isLoading);
+}
+
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
 }
 
 init();
