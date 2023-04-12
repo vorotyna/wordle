268553899fefe9622a3f803abc8a11ef576b0a92
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-04-12 12:26:36 -0700

    JavaScript: Winnign indicator

--- wordle.js
+++ wordle.js
@@ -8,7 +8,7 @@ async function init() {
   let currentRow = 0;
   let isLoading = true;
 
-  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day"); // OR "https://words.dev-apis.com/word-of-the-day?random=1" for a new word on each try
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
   const wordParts = word.split("");
@@ -83,6 +83,7 @@ async function init() {
     if (currentGuess === word) {
       // Win
       alert("You win!");
+      document.querySelector('.brand').classList.add('winner');
       done = true;
       return;
     } else if (currentRow === ROUNDS) {
